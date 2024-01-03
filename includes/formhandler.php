2c787d2359e8efe41fdf81ec37fe66c3df49453f
Anaïs <aekyon.08@gmail.com>
2024-01-03 10:54:06 +0100

    Add operation conversion from yen to euro

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -5,6 +5,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //grab data from the form in a secure way (prevent code injection)
     $amountToConvert = htmlspecialchars($_POST["amount"]);
 
-    echo "This is what the user submitted";
+    $yenToEuro = 1/156;
 
+    $amountConverted = $amountToConvert * $yenToEuro;
+
+    echo $amountToConvert . " yen" . " equals" . $amountConverted . " euros";
+
+    //empty field
+    if (empty($amountToConvert)) {
+        exit();
+        header ("Location: ../index.php");
+    }
+
+    //if not accessed through proper link
+} else {
+    header ("Location: ../index.php");
 }
