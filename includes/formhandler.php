720f368e8df33e23f22b2021af759f6b27988b54
Anaïs <aekyon.08@gmail.com>
2024-01-03 10:30:24 +0100

    Retrieve input data safely

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -1,1 +1,10 @@
 <?php 
+//check if form was accessed through post method
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    //grab data from the form in a secure way (prevent code injection)
+    $amountToConvert = htmlspecialchars($_POST["amount"]);
+
+    echo "This is what the user submitted";
+
+}
